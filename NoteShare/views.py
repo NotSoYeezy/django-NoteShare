a379b61f0891744a9b070b48a1527037c52b00be
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-08-15 16:48:21 +0200

    Adding infnite scroll to feed page.

--- NoteShare/views.py
+++ NoteShare/views.py
@@ -2,8 +2,9 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from accounts.models import User
 from notes.models import Note
-from random import shuffle
+from el_pagination.views import AjaxListView
 import random
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index_view(request):
@@ -11,10 +12,19 @@ def index_view(request):
 
     if request.user.is_authenticated:
         user = User.objects.get(pk=request.user.pk)
-        friends_usernames = user.friends
-        for username in friends_usernames:
-            friends.append(User.objects.get(username=username))
-    friends = random.sample(friends, len(friends))
+        if user.friends:
+            friends_usernames = user.friends
+            for username in friends_usernames:
+                friends.append(User.objects.get(username=username))
+            friends = random.sample(friends, len(friends))
+    page = request.GET.get('page', 1)
+    paginator = Paginator(friends, 2)
+    try:
+        friends = paginator.page(page)
+    except PageNotAnInteger:
+        friends = paginator.page(page)
+    except EmptyPage:
+        friends = paginator.page(paginator.num_pages)
     return render(request, 'index.html', {'friends': friends, 'notes': Note})
 
 
