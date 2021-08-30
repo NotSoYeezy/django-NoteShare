06a638fd4d45c50cf60bd0fdf50231889f81ba91
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-08-30 02:18:21 +0200

    Fixing tests.

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.hashers import make_password
 class ModelTest(TestCase):
 
     def test_creating_user_successful(self):
-        """Checking if user is creates
-            Can't test passwords because of using other hashers
-        """
+        """Checking if user is creates"""
         username = 'marcin'
         email = 'test@test.pl'
         password = 'test123'
 
-        user = get_user_model().objects.create(username=username, email=email, password=password)
+        user = get_user_model().objects.create(username=username, email=email)
+        user.set_password(password)
 
         self.assertEqual(user.email, email)
         self.assertEqual(user.username, username)
+        self.assertTrue(user.check_password(password))
