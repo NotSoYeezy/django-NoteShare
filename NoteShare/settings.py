1b7ed995b7f1c77c341edddf0029dfd04c70402a
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-08-31 14:26:05 +0200

    Fixing typo in database config

--- NoteShare/settings.py
+++ NoteShare/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
         'POST': '',
     }
 }
