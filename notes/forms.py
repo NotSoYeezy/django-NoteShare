73b62ffa269d63985a1bdde5eb4275fc10417bd0
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-10-24 02:30:03 +0200

    Adding max weight of file to note thumbnail and note file

--- notes/forms.py
+++ notes/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from .models import Note, Rate
+from .validators import check_file_size
 
 
 class NoteCreateForm(forms.ModelForm):
     # category = forms.ChoiceField(widget=forms.Select(attrs={'class': 'noteInput', 'id': 'cat_choice'}))
     title = forms.CharField(widget=forms.TextInput(attrs={'class': 'noteInput'}))
-    content_file = forms.FileField(widget=forms.FileInput(attrs={'id': 'file_field'}), required=False)
-    content_text = forms.CharField(widget=forms.Textarea(attrs={'id': 'note_text'}), required=False)
+    content_file = forms.FileField(widget=forms.FileInput(attrs={'id': 'file_field'}), required=False,
+                                   validators=[check_file_size])
+    content_text = forms.CharField(widget=forms.Textarea(attrs={'id': 'note_text'}), required=False,)
+    thumbnail = forms.ImageField(validators=[check_file_size])
 
     class Meta:
         model = Note
--- notes/validators.py
+++ notes/validators.py
@@ -8,3 +8,9 @@ def check_file_extension(file):
     accepted_extensions = ['.pdf', '.docx']
     if not extension.lower() in accepted_extensions:
         raise ValidationError('Unsupported file type.')
+
+
+def check_file_size(file):
+    """Checking if file exceeds max weight"""
+    if file.size > 2097152:  # 2 MB:
+        raise ValidationError('File is too big')
