99f8dd0ef48fedef0b3430dbcd926e7034b610ab
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-11-12 00:55:02 +0100

    Trying to fix prod error

--- NoteShare/settings.py
+++ NoteShare/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('MY_DEBUG_ENV_VAR') == 'True'
 
 ALLOWED_HOSTS = ['moja-witryna.com', 'localhost', '127.0.0.1', 'http://localhost:8000',
-                 'https://noteshare2021.herokuapp.com', '.herokuapp.com']
+                 'https://noteshare2021.herokuapp.com', '.herokuapp.com', '[::1]']
 
 # Application definition
 
