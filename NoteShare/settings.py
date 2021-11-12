d38392cb7cbb637b3facc9d7380eded12a453191
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-11-12 01:20:36 +0100

    Fixing DB error

--- NoteShare/settings.py
+++ NoteShare/settings.py
@@ -137,6 +137,8 @@ DATABASES = {
         'CONN_MAX_AGE': 500,
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
