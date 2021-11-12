d8a89621b433da53f464f68f0df2134a969f141a
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-11-12 01:00:15 +0100

    Trying to fix prod error v3

--- NoteShare/settings.py
+++ NoteShare/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('MY_DEBUG_ENV_VAR') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
