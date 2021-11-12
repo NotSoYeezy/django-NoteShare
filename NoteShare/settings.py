b264921fb5e55153c0474a5d0f3ef854a79e1b03
notsoyeezy <wojdalski.marcin2137@gmail.com>
2021-11-12 01:09:42 +0100

    Adding Debug True to identify issue

--- NoteShare/settings.py
+++ NoteShare/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['moja-witryna.com', 'localhost', '127.0.0.1', 'http://localhost:8000',
-                 'https://noteshare2021.herokuapp.com', '.herokuapp.com', '[::1]']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
